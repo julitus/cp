7caea6ca0cbcd06699bbacd48ebc4cd11c2d5512
Kalawasacraft <kalawasacraft@gmail.com>
2026-09-20 22:57:14 -0500

    hello world py cpp

---
+++ C++/hello.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FIN ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int main() {FIN
+	
+	cout << "Hello, World! C++\n";
+		
+	return 0;
+}
